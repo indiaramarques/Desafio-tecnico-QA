ce1c54e2abbdac4d2e3eb0159777fcd33a7c7315
Indiara Caetano Marques <76412627+indiaramarques@users.noreply.github.com>
2023-04-13 23:50:43 -0300

    🔧 Ajusta specifications

--- cypress/e2e/automacaoApi/Suite2.cy.js
+++ cypress/e2e/automacaoApi/Suite2.cy.js
@@ -4,14 +4,14 @@ describe('Suite 2 - Cenários Negativos', () => {
     it.only('API - Missing API Key', () => {
         cy.request({
             method: 'GET',
-            url: 'https://api.openweathermap.org/data/2.5/weather?lat=-23.6216986&lon=-11123&appid=b39b1cbac98954db9c513df26ecfb4dc&units=metric',
+            url: 'https://api.openweathermap.org/data/2.5/weather?lat=-23.6216986&lon=-46.8672509&appid=api_key_aqui&units=metric',
             failOnStatusCode: false,
         }).as('details')
 
-        cy.get('@details').its('status').should('eq', 400)
+        cy.get('@details').its('status').should('eq', 401)
         cy.get('@details').then((response) => {
             const err_message = JSON.stringify(response.body.message)
-            expect(err_message).to.equal('"wrong longitude"')
+            expect(err_message).to.equal('"Invalid API key. Please see https://openweathermap.org/faq#error401 for more info."')
         })
     })
 
